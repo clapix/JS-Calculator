71650621d8181221c3bb02ca853668ca7dc2e10f
claudia <mail@claudiapiccioni.it>
2020-04-15 15:56:43 +0200

    created some constant variables querying the data attributes, and created a Calculator class - and a constructor - to store the information I'll have

--- calculator.js
+++ calculator.js
@@ -1,1 +1,16 @@
-console.log("ciao")
+class Calculator {
+  constructor(previousTypedText, currentTypedText) {
+    this.previousTypedText = previousTypedText
+    this.currentTypedText = currentTypedText
+  }
+
+}
+
+
+const numberBtn = document.querySelectorAll("[data-num]")
+const operatorBtn = document.querySelectorAll("[data-operation]")
+const equalsBtn = document.querySelector("[data-equals]")
+const clearsBtn = document.querySelector("[data-clear]")
+const deleteBtn = document.querySelector("[data-delete]")
+const previousTypedText = document.querySelector("[data-previously-typed]")
+const currentTypedText = document.querySelector("[data-currently-typing]")
